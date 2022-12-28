a5b3c592cb7ce1d4bef028f8614815b33d84bedf
maleterian <uladzimir.bartkevich@gmail.com>
2022-12-28 19:09:55 +0100

    fixing sql after checking tasks description

--- src/sql/task2/2.5_total_earnings_pivot.sql
+++ src/sql/task2/2.5_total_earnings_pivot.sql
@@ -6,8 +6,29 @@ with tr as (
     where amount >= 0
     Group BY id, tr_year
 )
-Select *
+Select id,
+       ifnull( y_2011 , 0) as y_2011 ,
+       ifnull( y_2012 , 0) as y_2012 ,
+       ifnull( y_2013 , 0) as y_2013 ,
+       ifnull( y_2014 , 0) as y_2014 ,
+       ifnull( y_2015 , 0) as y_2015 ,
+       ifnull( y_2016 , 0) as y_2016 ,
+       ifnull( y_2017 , 0) as y_2017 ,
+       ifnull( y_2018 , 0) as y_2018 ,
+       ifnull( y_2019 , 0) as y_2019 ,
+       ifnull( y_2020 , 0) as y_2020 ,
+       ifnull( y_2021 , 0) as y_2021
 from tr Pivot (
     ifnull(  Round( Sum(amount), 2), 0 ) as total_amount
-    for tr_year in ( 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 )
+    for tr_year in ( 2011 as y_2011,
+                     2012 as y_2012,
+                     2013 as y_2013,
+                     2014 as y_2014,
+                     2015 as y_2015,
+                     2016 as y_2016,
+                     2017 as y_2017,
+                     2018 as y_2018,
+                     2019 as y_2019,
+                     2020 as y_2020,
+                     2021 as y_2021 )
 )
